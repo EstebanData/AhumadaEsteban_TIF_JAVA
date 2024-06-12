81fe4d44e55e55abd5fe9c044610caed46b5dfc9
Esteban <esteban-ahumada@hotmail.com>
2024-06-12 19:52:10 -0300

    Agregué los archivos finales

---
+++ src/main/java/com/ejercicios/MainApplication.java
@@ -0,0 +1,20 @@
+package com.ejercicios;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+
+
+public class MainApplication {
+
+    public static void main( String[] args ){
+	
+    
+
+
+
+ }}
+
